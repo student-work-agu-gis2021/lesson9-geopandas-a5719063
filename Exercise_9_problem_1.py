4c3aac2dba430e81043d2eeea07bc87858b06ac3
Nakagawa0208 <82347549+a5719063@users.noreply.github.com>
2021-06-13 22:52:58 +0000

    I added geo  plot and plt plot

--- Exercise_9_problem_1.py
+++ Exercise_9_problem_1.py
@@ -70,7 +70,7 @@ print(len(geo))
 geo.plot()
 plt.show()
 # What can you see on the map? :) 
-
+.
 # - save the GeoDataFrame into a Shapefile called `'polygon.shp'`. 
 
 # Save the GeoDataFrame into a new Shapefile called 'polygon.shp'.
